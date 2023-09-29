ad3fb3e976a521f928cf6bd245e1d8d44ce059bb
Kamilla Lopes <105080698+KamillaLopes@users.noreply.github.com>
2023-09-29 11:20:40 -0300

    ok

--- main.js
+++ main.js
@@ -3,3 +3,43 @@ function tocaSomPom () {
 }
 
 document.querySelector('.tecla_pom');
+
+
+
+function tocaSomClap () {
+    document.querySelector('#som_tecla_clap').play();
+}
+
+document.querySelector('.tecla_clap');
+
+
+
+function tocaSomTim () {
+    document.querySelector('#som_tecla_tim').play();
+}
+
+document.querySelector('.tecla_tim');
+
+
+
+function tocaSomPuff () {
+    document.querySelector('#som_tecla_puff').play();
+}
+
+document.querySelector('.tecla_puff');
+
+
+
+function tocaSomSplash () {
+    document.querySelector('#som_tecla_splash').play();
+}
+
+document.querySelector('.tecla_splash');
+
+
+
+function tocaSomToim () {
+    document.querySelector('#som_tecla_toim').play();
+}
+
+document.querySelector('.tecla_toim');
