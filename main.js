809114b16b5a04e6c45e33f2fe010b632d85b231
Kamilla Lopes <105080698+KamillaLopes@users.noreply.github.com>
2023-10-20 11:18:31 -0300

    ok

--- main.js
+++ main.js
@@ -3,4 +3,61 @@ function tocaSomPom () {
     document.querySelector('#som_tecla_pom').play();
 }
 
-const listaDeTeclas = document.querySelectorAll('.tecla');
+document.querySelector('.tecla_pom').onclick = tocaSomPom;
+
+//Declaração/criação da função tocaSomClap
+function tocaSomClap () {
+    document.querySelector('#som_tecla_clap').play();
+}
+
+document.querySelector('.tecla_clap').onclick = tocaSomClap;
+
+//Declaração/criação da função tocaSomTom
+function tocaSomTim () {
+    document.querySelector('#som_tecla_tim').play();
+}
+
+document.querySelector('.tecla_tim').onclick = tocaSomTim;
+
+//Declaração/criação da função tocaSomPuff
+function tocaSomPuff () {
+    document.querySelector('#som_tecla_puff').play();
+}
+
+document.querySelector('.tecla_puff').onclick = tocaSomPuff;
+
+//Declaração/criação da função tocaSomSplash
+function tocaSomSplash () {
+    document.querySelector('#som_tecla_splash').play();
+}
+
+document.querySelector('.tecla_splash').onclick = tocaSomSplash;
+
+//Declaração/criação da função tocaSomToim
+function tocaSomToim () {
+    document.querySelector('#som_tecla_toim').play();
+}
+
+document.querySelector('.tecla_toim').onclick = tocaSomToim;
+
+//Declaração/criação da função tocaSomPsh
+function tocaSomPsh () {
+    document.querySelector('#som_tecla_psh').play();
+}
+
+document.querySelector('.tecla_psh').onclick = tocaSomPsh;
+
+//Declaração/criação da função tocaSomTic
+function tocaSomTic () {
+    document.querySelector('#som_tecla_tic').play();
+}
+
+document.querySelector('.tecla_tic').onclick = tocaSomTim;
+
+//Declaração/criação da função tocaSomTom
+function tocaSomTom () {
+    document.querySelector('#som_tecla_tom').play();
+}
+
+document.querySelector('.tecla_tom').onclick = tocaSomTom;
+
