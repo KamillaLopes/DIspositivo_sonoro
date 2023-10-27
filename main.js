70d5a66a085995018ccd8dca6f2763e93bac7b2c
Kamilla Lopes <105080698+KamillaLopes@users.noreply.github.com>
2023-10-27 10:47:45 -0300

    ok

--- main.js
+++ main.js
@@ -1,14 +1,16 @@
 //Declaração/criação da função tocaSomPom
-function tocaSomPom () {
-    document.querySelector('#som_tecla_pom').play();
+function tocaSom(idElementoAudio) {
+    document.querySelector(idElementoAudio).play();
 }
 
 //criando a referência constante listaDeTeclas e buscando a classe coletiva .tecla
 const listaDeTeclas = document.querySelectorAll('.tecla');
 
 let contador = 0;
-while(contador<9){
-    listaDeTeclas[0].onclick = tocaSomPom;
+while(contador < listaDeTeclas.length){
+    listaDeTeclas[contador].onclick = function(){
+        tocaSom(idAudio);
+    }
     contador = contador + 1;
     console.log(contador);
 }
